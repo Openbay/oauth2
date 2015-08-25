7af8772e0f5fdc3e4c2485f5ed54e0a8e12f81bc
Jimmy Perrine <jimmy.perrine@gmail.com>
2015-08-25 16:37:11 -0400

    allow deletion of params so we can nuke out client_id and client_secret for third party oauth requirements.

--- lib/oauth2/strategy/auth_code.rb
+++ lib/oauth2/strategy/auth_code.rb
@@ -26,6 +26,9 @@ module OAuth2
       # @note that you must also provide a :redirect_uri with most OAuth 2.0 providers
       def get_token(code, params = {}, opts = {})
         params = {'grant_type' => 'authorization_code', 'code' => code}.merge(client_params).merge(params)
+        if block_given?
+          yield params
+        end
         @client.get_token(params, opts)
       end
     end
